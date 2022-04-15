f60fa2902acd675070a119e304aba7641b6a509d
rasak <rasak@systemspecs.com.ng>
2022-04-15 01:59:47 +0100

    add logs to my test class

--- src/test/java/com/hospital/assessment/HospitalApplicationTests.java
+++ src/test/java/com/hospital/assessment/HospitalApplicationTests.java
@@ -1,6 +1,7 @@
 package com.hospital.assessment;
 
 import com.hospital.assessment.dto.StaffRequest;
+import com.hospital.assessment.firstTest.HospitalApplicationSecondTests;
 import com.hospital.assessment.model.Staff;
 import com.hospital.assessment.repository.StaffRepository;
 import com.hospital.assessment.service.HospitalServiceImpl;
@@ -12,6 +13,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -19,12 +22,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
+import java.util.Optional;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HospitalApplicationTests {
 
+    private static final Logger logger = LoggerFactory.getLogger(HospitalApplicationTests.class);
 
 
  @Autowired
@@ -55,7 +60,9 @@ public class HospitalApplicationTests {
         @Test
     public void createStaffTest() {
         staffRepository.save(staffRequest);
-        Assert.assertNotNull(staffRepository.findByUuid(uuid));
+            Optional<Staff> byUuid = staffRepository.findByUuid(uuid);
+            logger.info("<<< RESPONSE >>>> {}", byUuid);
+            Assert.assertNotNull(byUuid);
     }
 
 
